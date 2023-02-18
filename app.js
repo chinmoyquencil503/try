99f93ffdcce22adbd27ef9eb558437f9c1a1ed0b
chinmoy-quencil <124921057+chinmoyquencil503@users.noreply.github.com>
2023-02-18 22:04:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-require("../connection/connc");
+require("./connection/connc");
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
